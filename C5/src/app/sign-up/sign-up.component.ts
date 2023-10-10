d6fb8401491a485db52dd06e446e486f6759b00e
LAURA_MAYA DD <mayadray1@gmail.com>
2023-10-10 22:16:18 +0200

    exercice done

--- C5/src/app/sign-up/sign-up.component.ts
+++ C5/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  isSend = false
+
+  user: User = new User("", "", "", "");
+
+  constructor() { }
+
+  public onSubmit(): void {
+    console.log(this.user)
+    this.isSend = true
+  }
+
 }
+
+
+
+export class User {
+  constructor(
+    public email: string,
+    public firstname: string,
+    public lastname: string,
+    public password: string
+  ) { }
+
+}
+
+
